b7b6774125906683567f40a1a313f70926aeb2c4
Seraphin Elias Rihm <seraphin.rihm@edu.tbz.ch>
2017-05-30 14:44:28 +0200

    #UpdateMethodOnServerIsImplementedNow

--- SchinkZeShips.Server/ISchinkZeShips.cs
+++ SchinkZeShips.Server/ISchinkZeShips.cs
@@ -20,5 +20,8 @@ namespace SchinkZeShips.Server
 
 		[OperationContract]
 		void RemoveFromGame(string gameId, string playerId);
+
+        [OperationContract]
+        void UpdateCurrentGame(Game currentGame);
 	}
 }
