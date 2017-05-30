e46de3ca0c697523288716cc60285f779987a8c7
Seraphin Elias Rihm <seraphin.rihm@edu.tbz.ch>
2017-05-30 16:17:59 +0200

    #FirstLogicStuff

---
+++ SchinkZeShips.Core/GameLogic/ValidateBoard.cs
@@ -0,0 +1,25 @@
+﻿using SchinkZeShips.Core.SchinkZeShipsReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchinkZeShips.Core.GameLogic
+{
+	class ValidateBoard
+	{
+
+		public PlayingFieldState lastGameState;
+
+		public PlayingFieldState validateBoard(PlayingFieldState board)
+		{
+			return new PlayingFieldState();
+		}
+
+		public ValidateBoard(PlayingFieldState board)
+		{
+			this.lastGameState = board;
+		}
+	}
+}
